695a876482a6fef98a6a62ee43c93b99506fc0a8
shihab <shihab456321@gmail.com>
2021-05-24 06:20:09 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import NavInshort from "./components/NavInshort";
+import NewsContent from "./components/NewsContent/NewsContent";
+//import apikey from './data/config';
+
+
+function App() {
+  const [newsArray, setNewsArray] = useState([]);
+  const [newsResults, setNewsResults] = useState();
+  const [loadMore, setLoadMore] = useState(20);
+  const [category, setCategory] = useState("general");
+
+  console.log(process.env);
+
+  const newsApi = async () => {
+    try {
+      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+
+      const news = await axios.get(
+        //
+        `https://${proxyUrl}newsapi.org/v2/top-headlines?country=in&apiKey=${"3a5c618908644ab9bf6d255282337d27"}&pageSize=${loadMore}&category=${category}`
+      );
+       console.log(news);
+      setNewsArray(news.data.articles);
+      setNewsResults(news.data.totalResults);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    newsApi();
+    // eslint-disable-next-line
+  }, [newsResults, loadMore, category]);
+
+  return (
+    <div className="App" id="#home">
+      <NavInshort setCategory={setCategory} />
+      {newsResults && (
+        <NewsContent
+          newsArray={newsArray}
+          newsResults={newsResults}
+          loadMore={loadMore}
+          setLoadMore={setLoadMore}
+        />
+      )}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/config.js
@@ -0,0 +1,2 @@
+const apikey = "3a5c618908644ab9bf6d255282337d27"
+export default apikey;
